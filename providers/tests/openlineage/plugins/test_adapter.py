badbc90b61779612034267120552fcf3f166884f
Ankit Chaurasia <8670962+sunank200@users.noreply.github.com>
2024-10-06 21:48:29 +0530

    Fix more tests

--- providers/tests/openlineage/plugins/test_adapter.py
+++ providers/tests/openlineage/plugins/test_adapter.py
@@ -882,7 +882,7 @@ def test_build_task_instance_run_id_is_valid_uuid():
         dag_id="dag_id",
         task_id="task_id",
         try_number=1,
-        execution_date=datetime.datetime.now(),
+        logical_date=datetime.datetime.now(),
     )
     uuid_result = uuid.UUID(result)
     assert uuid_result
@@ -894,13 +894,13 @@ def test_build_task_instance_run_id_same_input_gives_same_result():
         dag_id="dag1",
         task_id="task1",
         try_number=1,
-        execution_date=datetime.datetime(2024, 1, 1, 1, 1, 1),
+        logical_date=datetime.datetime(2024, 1, 1, 1, 1, 1),
     )
     result2 = OpenLineageAdapter.build_task_instance_run_id(
         dag_id="dag1",
         task_id="task1",
         try_number=1,
-        execution_date=datetime.datetime(2024, 1, 1, 1, 1, 1),
+        logical_date=datetime.datetime(2024, 1, 1, 1, 1, 1),
     )
     assert result1 == result2
 
@@ -910,13 +910,13 @@ def test_build_task_instance_run_id_different_inputs_gives_different_results():
         dag_id="dag1",
         task_id="task1",
         try_number=1,
-        execution_date=datetime.datetime.now(),
+        logical_date=datetime.datetime.now(),
     )
     result2 = OpenLineageAdapter.build_task_instance_run_id(
         dag_id="dag2",
         task_id="task2",
         try_number=2,
-        execution_date=datetime.datetime.now(),
+        logical_date=datetime.datetime.now(),
     )
     assert result1 != result2
 
